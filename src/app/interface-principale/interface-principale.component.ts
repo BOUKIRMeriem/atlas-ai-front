461100ca400b30bc429346da0c1722f0679c742f
BOUKIR Meriem <meriemboukir26@example.com>
2025-03-19 01:03:15 +0000

    .

--- src/app/interface-principale/interface-principale.component.ts
+++ src/app/interface-principale/interface-principale.component.ts
@@ -8,23 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./interface-principale.component.scss']
 })
 export class InterfacePrincipaleComponent {
-  message: string = '';
-  messages: { sender: string, text: string, timestamp: Date }[] = [];
 
-
-  // Méthode pour envoyer un message
-  sendMessage() {
-    if (!this.message.trim()) return;
-
-    // Ajouter le message de l'utilisateur
-    this.messages.push({ sender: 'user', text: this.message, timestamp: new Date() });
-
-    // Ajouter une réponse automatique après un délai
-    setTimeout(() => {
-      this.messages.push({ sender: 'bot', text: 'Réponse automatique du bot.', timestamp: new Date() });
-    }, 1000);
-
-    // Réinitialiser le champ de message
-    this.message = '';
-  }
 }
